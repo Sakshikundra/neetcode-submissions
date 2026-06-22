354cc6833aae56ce3d540f16649e2294d77315fb
Sakshi kundra <136843042+Sakshikundra@users.noreply.github.com>
2026-06-22 13:09:21 +0530

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private :
+    int solve(int amount , vector <int>& coins, vector<int>& dp){
+        if (amount==0)
+        return 0;
+        if (amount<0)
+        return 1e9;
+        if (dp[amount]!=-1){
+            return dp[amount];
+
+        }
+        int ans=1e9;
+
+        for (int coin:coins){
+            ans=min(ans,1+ solve(amount-coin,coins,dp));
+
+        }
+
+        return dp[amount]=ans;
+    }
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1,-1);
+        int ans =solve(amount,coins,dp);
+        return (ans>=1e9)?-1:ans;
+    }
+};
